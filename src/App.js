6bc945fb3fa954493cab9774a7452c653abcebac
lemur <denisgevkach@gmail.com>
2025-06-06 18:21:33 +0300

    Added adaptive styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import RoomsPage from "./components/RoomsPage";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
-    <BrowserRouter basename="/lemur-lodge">
+    <HashRouter>
+      {/* Додаємо ScrollToTop */}
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rooms" element={<RoomsPage />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
-
 
 export default App;
--- src/components/menu/Header.js
+++ src/components/menu/Header.js
@@ -8,23 +8,21 @@ const Menu = () => {
     return (
         <Navbar className="custom-navbar">
             <Container>
-                <Navbar.Brand className="navbar-brand-custom" href="/lemur-lodge/">Lemur Lodge</Navbar.Brand>
+                <Navbar.Brand className="navbar-brand-custom" href="#/">Lemur Lodge</Navbar.Brand>
                 <Nav className="ms-auto">
-                    <Nav.Link href="/lemur-lodge/">Home</Nav.Link>
-                    <Nav.Link href="/lemur-lodge/rooms">Rooms</Nav.Link>
-                    <Nav.Link href="/lemur-lodge/about">About us</Nav.Link>
+                    <Nav.Link href="#/">Home</Nav.Link>
+                    <Nav.Link href="#/rooms">Rooms</Nav.Link>
+                    <Nav.Link href="#/about">About us</Nav.Link>
                     
-                    <div className="dropdown-wrapper" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
-                        <Nav.Link>Phone Numbers</Nav.Link>
-                        {showDropdown && (
-                            <div className="dropdown-menu">
-                                <p>📞 +380673550XXX</p>
-                                <p>📞 +380931816XXX</p>
-                            </div>
-                        )}
-                    </div>
-                </Nav>
-                <Navbar.Toggle />
+                    <div className="dropdown-wrapper" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}> 
+                        <Nav.Link>Phone Numbers</Nav.Link> 
+                        {showDropdown && ( <div className="dropdown-menu">
+                             <p>📞 +380673550XXX</p> 
+                             <p>📞 +380931816XXX</p>
+                              </div> )} 
+                              </div> 
+                              </Nav> 
+                              <Navbar.Toggle />
             </Container>
         </Navbar>
     );
